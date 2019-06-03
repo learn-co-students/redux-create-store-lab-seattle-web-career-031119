a3f0d1ef1ca40c5e6964ffda1c3c996e5d0a56cc
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-06-03 11:43:32 -0700

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,38 @@
+import candyReducer from './reducers/candyReducer'
+import countReducer from './reducers/countReducer'
+
 export default function createStore(reducer) {
-  // add your code here
+  let state
+
+  const dispatch = action => {
+    state = reducer(state, action)
+    render()
+  }
+
+  const getState = () => {
+    return state
+  }
+
+  const addCandy = (state, action) => {
+    switch (action.type) {
+      case 'ADD_CANDY':
+        return [...state, candy]
+      default:
+        return state
+    }
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+//
+// let store = createStore(candyReducer)
+// store.dispatch({type: '@@INIT'})
